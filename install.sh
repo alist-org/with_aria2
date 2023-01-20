2621f6cdd9cb3e047d65df123881f923c49bb098
Noah Hsu <i@nn.ci>
2023-01-21 00:49:00 +0800

    touch /root/.aria2/aria2.session

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ wget https://github.com/P3TERX/aria2.conf/archive/refs/heads/master.tar.gz
 tar -zxvf master.tar.gz --strip-components=1
 rm -rf master.tar.gz
 sed -i 's|rpc-secret|#rpc-secret|g' ./aria2.conf
+touch /root/.aria2/aria2.session
 ./tracker.sh
