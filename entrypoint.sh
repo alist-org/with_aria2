79f8555a754abd42e309d8c519b7f70026d4a34a
Noah Hsu <i@nn.ci>
2023-01-21 00:30:30 +0800

    change aria2 position

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ umask ${UMASK}
 exec su-exec ${PUID}:${PGID} nohup aria2c \
   --enable-rpc \
   --rpc-allow-origin-all \
-  --conf-path=/opt/alist/data/aria2/aria2.conf \
+  --conf-path=/root/.aria2/aria2.conf \
   >/dev/null 2>&1 &
 
 exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apk add --no-cache aria2 curl
 
-mkdir -p /opt/alist/data/aria2
-cd /opt/alist/data/aria2
+mkdir -p /root/.aria2
+cd /root/.aria2
 wget https://github.com/P3TERX/aria2.conf/archive/refs/heads/master.tar.gz
 tar -zxvf master.tar.gz --strip-components=1
 rm -rf master.tar.gz
