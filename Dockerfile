87a2e1897af258392871f867f0306394d95516bf
Noah Hsu <i@nn.ci>
2023-01-21 00:14:24 +0800

    add aria2.conf from `P3TERX` (close #1)

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ LABEL MAINTAINER="i@nn.ci"
 VOLUME /opt/alist/data/
 WORKDIR /opt/alist/
 COPY entrypoint.sh /entrypoint.sh
-RUN apk add --no-cache aria2; \
-    chmod +x /entrypoint.sh
-    
+COPY install.sh /install.sh
+RUN chmod +x /entrypoint.sh /install.sh; \
+  /install.sh
+
 ENV PUID=0 PGID=0 UMASK=022
 EXPOSE 5244
 ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@ chown -R ${PUID}:${PGID} /opt/alist/
 
 umask ${UMASK}
 
-exec su-exec ${PUID}:${PGID} nohup aria2c --enable-rpc --rpc-allow-origin-all > /dev/null 2>&1 &
+exec su-exec ${PUID}:${PGID} nohup aria2c \
+  --enable-rpc \
+  --rpc-allow-origin-all \
+  --conf-path=/opt/alist/data/aria2/aria2.conf \
+  >/dev/null 2>&1 &
 
 exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
